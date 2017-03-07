f50d27926cc4f56991f1d1e4b87858faab1b25ff
icemarx <matjaz.ciglic.jst@gmail.com>
2017-03-07 15:10:18 +0100

    Create QuickTypeDemo.java

---
+++ QuickTypeDemo.java
@@ -0,0 +1,13 @@
+public class QuickTypeDemo{
+  public static void main(String []args){
+    Scanner in = new Scanner(System.in)
+    
+    // input
+    String input = in.nextLine();
+    if(input.length() > 4){
+      input = input.substring(0,4);
+    }
+    
+    // processing
+  }
+}
