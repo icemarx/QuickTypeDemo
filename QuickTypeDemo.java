6c55e6fa6110a967f7f819147359e066c234355a
icemarx <matjaz.ciglic.jst@gmail.com>
2017-03-09 21:44:52 +0100

    Finished the basic code.
    Program still needs calibration and a longer alphabet array.
    Now it requires UseFile sub-program, to deal with files.
    It can already function as a very primitive keyboard.
    At this state, the program is currently debugged.

--- QuickTypeDemo.java
+++ QuickTypeDemo.java
@@ -3,49 +3,62 @@ import java.util.Scanner;
 
 public class QuickTypeDemo{
 	public static void main(String []args){
-    	Scanner in = new Scanner(System.in);
-    
-    	//calibration
-    	String[] keys = {"q", "w", "e", "r", "v"}; // this is for testing, it will be calibrated in later versions
-    	String[] alphabet = {".", " ", "a", "b", "c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
-    
-    	/*
-    	 * insert open file code here
-    	 */
-    
-    	outerloop:
-    	while(true){
-	      	try{
-	        	// input
-	        	String input = in.nextLine();
-	        	if(input.length() > 5)
-	        		input = input.substring(0,5);
+    		Scanner in = new Scanner(System.in);
+    		UseFile file = new UseFile();
+    		boolean run;
+
+    		// ask for file
+    		System.out.println("Enter file name (without .txt): ");
+    		String file_name = in.nextLine();
+    	
+    		try{
+    			file.openFile(file_name);
+			run = true;
+		}catch(Exception e){
+			run = false;
+		}
+    	
+	    	// calibration
+    		String[] keys = {"q", "w", "e", "r", "v"}; // this is for testing, it will be calibrated in later versions
+    		String[] alphabet = {"exit", " ", "a", "b", "c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+
+    		outerloop:
+    		while(run){
+		      	try{
+	        		// input
+	        		String input = in.nextLine();
+	        		if(input.length() > 5)
+	        			input = input.substring(0,5);
 	    
-	        	// initialise
-	        	int sum = 0;
+		        	// initialise
+		        	int sum = 0;
 	    	
-	    	    // check letters
-	    	    for(int i = 0; i < keys.length; i++){
-	    	    if(input.contains(keys[i]))
-	        	    sum += Math.pow(2, i);
-	       		 }
+	    			// check letters
+	    	    		for(int i = 0; i < keys.length; i++){
+	    	    			if(input.contains(keys[i]))
+	        	    			sum += Math.pow(2, i);
+	       		 	}
 	    
-	        	// add letter to string and check for break
-	        	if(alphabet[sum] == ".")
-	        		break outerloop;
-	        	else
-	        		try{
-	         			// output
-	        			System.out.println(alphabet[sum]); // will change to add to a .txt file
-	        		}catch(Exception e){
-	            		System.out.println("error while loading to file"); // this will print when the file output will be implemented and it won't work
-	            		break outerloop;
+	        		// add letter to string and check for break
+	        		if(alphabet[sum] == "exit")
+	        			break outerloop;
+	        		else{
+	        			try{
+	         				// output
+		        			System.out.println(alphabet[sum]); // will change to add to a .txt file
+		        			file.addData(alphabet[sum]);
+	        			}catch(Exception e){
+	            				System.out.println("error while loading to file"); // this will print when the file output will be implemented and it won't work
+	            				break outerloop;
+					}
 	        		}
-	    		}catch(Exception e){
-	    	}
-    	}
-    /*
-   	 * insert close file code here
-   	 */
+	    		}catch(Exception e){}
+    		}
+    		try{
+			// close file    
+			file.closeFile();
+		}catch(Exception e){
+			System.out.println("Error closing file.");
+		}
   	}
 }
