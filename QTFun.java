16d6951c8f728fbff3d392ac35af06ce17b68a91
icemarx <matjaz.ciglic.jst@gmail.com>
2017-03-29 16:04:03 +0200

    Update QTFun.java

--- QTFun.java
+++ QTFun.java
@@ -5,10 +5,10 @@ public class QTFun{
 	private Scanner in = new Scanner(System.in);
 
 	public int Calibration(int cal){
-			if(cal < 4 || cal > 10)
-				cal = 5;
-			System.out.println("Calibration set to " + cal);
-			return cal;
+		if(cal < 4 || cal > 10)
+			cal = 5;
+		System.out.println("Calibration set to " + cal);
+		return cal;
 	}
 
 	public String[] KeySelect(int cal){
@@ -35,17 +35,17 @@ public class QTFun{
 
 		// check letters
 		for(int i = 0; i < keys.length; i++){
-		if(input.contains(keys[i]))
-	    		sum += Math.pow(2, i);
-	}
-	return sum;
+			if(input.contains(keys[i]))
+	    			sum += Math.pow(2, i);
+		}
+		return sum;
 	}
 
 	public void QTmain(UseFile file){
 		int calibration = 5;
 		// String[] alphabet = {	" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ".", ",", ":", "exit", "--linebr--"};	// \n apparently doesnt work
 		String[] alphabet = {	" ", "e", "o", "s", "t", "i", "d", "f", "h", "n", "l", "g", "y", "b", "v", "z", 
-								"a", "r", "u", "c", "m", "p", "j", ".", "w", "k", "q", ",", "x", ":", "EXIT", "\n"};	// alphabet based on frequency in use (source: https://en.wikipedia.org/wiki/Letter_frequency; 11.3.2017, 9:55)
+					"a", "r", "u", "c", "m", "p", "j", ".", "w", "k", "q", ",", "x", ":", "EXIT", "\n"};	// alphabet based on frequency in use (source: https://en.wikipedia.org/wiki/Letter_frequency; 11.3.2017, 9:55)
 		String[] keys;
 		try{
 			System.out.print("Calibrate number: ");
