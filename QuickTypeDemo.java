09da1b96a80f01a1cf3f9dce0e0d4cd97386ae88
icemarx <matjaz.ciglic.jst@gmail.com>
2017-03-29 15:52:08 +0200

    Update QuickTypeDemo.java
    Fixed some whitespace

--- QuickTypeDemo.java
+++ QuickTypeDemo.java
@@ -2,29 +2,29 @@ import java.util.Scanner;
 
 public class QuickTypeDemo{
 	public static void main(String []args){
-    	Scanner in = new Scanner(System.in);
-    	UseFile file = new UseFile();
-   		QTFun fun = new QTFun();
-   		boolean run = true;
-    	// ask for file
-    	System.out.print("Enter file name (without .txt): ");
-   		String file_name = in.nextLine().replaceAll("\\s+", "");
-   	
-   		try{
-   			file.openFile(file_name);
-   		}catch(Exception e){
-			run = false;
-   		}
-    	
-    	// main part
-    	if(run)
-    		fun.QTmain(file);
+		Scanner in = new Scanner(System.in);
+		UseFile file = new UseFile();
+		QTFun fun = new QTFun();
+		boolean run = true;
+		// ask for file
+		System.out.print("Enter file name (without .txt): ");
+		String file_name = in.nextLine().replaceAll("\\s+", "");
 
-    	try{
-			// close file    
+		try{
+			file.openFile(file_name);
+		}catch(Exception e){
+			run = false;
+		}
+
+		// main part
+		if(run)
+		fun.QTmain(file);
+
+		// close file
+		try{
 			file.closeFile();
 		}catch(Exception e){
 			System.out.println("Error closing file.");
 		}
-  	}
+	}
 }
